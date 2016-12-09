5d658d6273c36f7df399225657e3323379077984
SAKIB RAHMAN <srskb15@gmail.com>
2016-12-09 22:50:59 +0600

    Initial Commit

---
+++ VirtualJudge/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace VirtualJudge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroDateTime1 = new MetroFramework.Controls.MetroDateTime();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // metroDateTime1
+            // 
+            resources.ApplyResources(this.metroDateTime1, "metroDateTime1");
+            this.metroDateTime1.Name = "metroDateTime1";
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.metroComboBox1, "metroComboBox1");
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.UseSelectable = true;
+            // 
+            // metroButton1
+            // 
+            resources.ApplyResources(this.metroButton1, "metroButton1");
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // webBrowser1
+            // 
+            resources.ApplyResources(this.webBrowser1, "webBrowser1");
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroComboBox1);
+            this.Controls.Add(this.metroDateTime1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroDateTime metroDateTime1;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ VirtualJudge/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Net;
+
+namespace VirtualJudge
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("http://codeforces.com/problemset");
+            int i = 0;
+            foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//table/tr"))
+            {
+                i++;
+                //Console.WriteLine("row " + i);
+
+                foreach (HtmlNode cell in table.SelectNodes("//td[2]/div[1]"))
+                {
+                    //Console.WriteLine("cell: " + cell.InnerText);
+                    webBrowser1.DocumentText = cell.InnerText;
+                }
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, System.Windows.Forms.WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
